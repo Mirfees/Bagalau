15d9180f9b543cb99a072167f10a7f5a979268e3
mirfees <mirfees@gmail.com>
2024-02-26 19:04:06 +0600

    заканчиваю разработку главной страницы

--- Bagalau/gulpfile.js
+++ Bagalau/gulpfile.js
@@ -20,7 +20,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+//import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -31,8 +31,8 @@ function watcher() {
     gulp.watch(path.watch.images, images);
 }
 
-const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images))
+// const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
+const mainTasks = gulp.series(/*fonts,*/ gulp.parallel(copy, html, scss, js, images))
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
